79cd5848ea0cbc284af5ce38956fca34317b90f1
idou-dev <idouyoussef87@gmail.com>
2023-12-12 15:48:02 -0800

    main file

---
+++ main.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+
+/**
+ * main - the entry function
+ * @ac: argument counter
+ * @av: argument vector
+ * Return: 0 on success
+ */
+
+int main(int ac, char **av)
+{
+	char *line = NULL, **command = NULL;
+	int status = 0;
+	(void)ac;
+	(void)av;
+
+	while (1)
+	{
+		line = r_line();
+		if (line == NULL)
+		{
+			if (isatty(STDIN_FILENO) == 1)
+				write(STDOUT_FILENO, "\n", 1);
+			return (status);
+		}
+		command = token(line);
+		if (!command)
+			continue;
+	}
+	return (0);
+}
